67fbfc0b18fde9ac7e90f65d6144380bd6a15cdb
mojyack <mojyack@gmail.com>
2024-05-11 15:49:58 +0900

    edl-buse: use BlockOperator

--- src/edl-buse.cpp
+++ src/edl-buse.cpp
@@ -1,79 +1,23 @@
-#include <vector>
+#include <array>
 
 #include "assert.hpp"
+#include "buse/block-operator.hpp"
 #include "buse/buse.hpp"
 #include "firehose-actions.hpp"
 #include "serial-device.hpp"
 #include "util/charconv.hpp"
 
 namespace {
-template <class T, class U>
-auto align(const T value, const U unit) -> T {
-    return (value / unit) * unit;
-}
-
-struct EDLOperator : buse::Operator {
+struct EDLOperator : buse::BlockOperator {
     Device* dev;
     int     disk;
 
-    auto aligned_io(const bool write, void* const buf, const size_t len, const size_t offset) -> int {
-        const auto block  = offset / block_size;
-        const auto blocks = len / block_size;
-
-        auto r = true;
-        if(write) {
-            r = fh::write_disk(*dev, disk, block, blocks, std::bit_cast<std::byte*>(buf));
-        } else {
-            r = fh::read_disk(*dev, disk, block, blocks, std::bit_cast<std::byte*>(buf));
-        }
-        return r ? 0 : EIO;
+    auto read_block(void* buf, const size_t blocks, const size_t block) -> int override {
+        return fh::read_disk(*dev, disk, block, blocks, std::bit_cast<std::byte*>(buf)) ? 0 : EIO;
     }
 
-    // a a a a b b b b c c c c
-    // 0 1 2 3 4 5 6 7 8 9 a b
-    // 7 3           I____
-    //         I
-    // 4 5     I________
-    //         I
-    auto generic_io(const bool write, void* const buf, const size_t len, const size_t offset) -> int {
-        if(len % block_size == 0 && offset % block_size == 0) {
-            return aligned_io(write, buf, len, offset);
-        } else {
-            const auto aligned_offset = align(offset, block_size);
-            const auto offset_gap     = offset - aligned_offset;
-            const auto block          = aligned_offset / block_size;
-            const auto blocks         = ((len + offset_gap) + block_size - 1) / block_size;
-            auto       tmp            = std::vector<std::byte>(blocks * block_size);
-            if(write) {
-                if(offset_gap != 0 && !fh::read_disk(*dev, disk, block, 1, tmp.data())) {
-                    return EIO;
-                }
-                const auto last_block = block + blocks - 1;
-                if((len - (block_size - offset_gap)) % block_size != 0 &&
-                   !fh::read_disk(*dev, disk, last_block, 1, tmp.data() + (blocks - 1) * block_size)) {
-                    return EIO;
-                }
-                memcpy(tmp.data() + offset_gap, buf, len);
-                if(!fh::write_disk(*dev, disk, block, blocks, tmp.data())) {
-                    return EIO;
-                }
-                return 0;
-            } else {
-                if(!fh::read_disk(*dev, disk, block, blocks, tmp.data())) {
-                    return EIO;
-                }
-                memcpy(buf, tmp.data() + offset_gap, len);
-                return 0;
-            }
-        }
-    }
-
-    auto read(void* buf, const size_t len, const size_t offset) -> int override {
-        return generic_io(false, buf, len, offset);
-    }
-
-    auto write(const void* buf, size_t len, size_t offset) -> int override {
-        return generic_io(true, const_cast<void*>(buf), len, offset);
+    auto write_block(const void* buf, size_t blocks, size_t block) -> int override {
+        return fh::write_disk(*dev, disk, block, blocks, std::bit_cast<std::byte*>(buf)) ? 0 : EIO;
     }
 
     auto disconnect() -> int override {
